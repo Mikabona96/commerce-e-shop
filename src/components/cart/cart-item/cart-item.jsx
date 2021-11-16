72cd2f63cc54afd0ad536272b6d0a63f1aa81894
Mika <dmitriygolubapk@gmail.com>
2021-11-16 17:02:16 +0300

    cart-item creareated, connected to cartdropdown

---
+++ src/components/cart/cart-item/cart-item.jsx
@@ -0,0 +1,13 @@
+import './cart-item.scss'
+
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
+	<div className="cart-item">
+		<img src={imageUrl} alt="" />
+		<div className="item-details">
+			<span className="name">{name}</span>
+			<span className="price">{quantity ? quantity : null} {quantity ? 'x' : null} ${price}</span>
+		</div>
+	</div>
+)
+
+export default CartItem
