c826e52491eac3b87fb001dfc775813fdd6a7bf8
Mika <dmitriygolubapk@gmail.com>
2021-11-16 13:06:55 +0300

    user redirection to main page when logged in

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -1,8 +1,17 @@
 import './sign-in-and-sign-up.scss'
 import SignIn from '../../components/sign-in/sign-in'
 import SignUp from '../../components/sign-up/sign-up'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router'
+
 
 const SignInAndSignUpPage = () => {
+	const navigate = useNavigate()
+	const currentUser = useSelector(state => state.user.currentUser)
+
+	if (currentUser) {
+		setTimeout(() => navigate('/'), 100)
+	}
 	return (
 		<div className="sign-in-and-sign-up">
 			<SignIn />
